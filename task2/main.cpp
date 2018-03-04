30121232edf3885e346284185177f0783716163f
Lenar Gilyazov <lenar8553@gmail.com>
2018-03-04 16:48:21 +0300

    Исправил 2ое задание

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
 #include <omp.h>
 
-void task1()
-{
-    #pragma omp parallel num_threads(3)
-    {
-        #pragma omp parallel num_threads(3) if(omp_get_num_threads() > 1)
-        {
-            printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
-        }
-    }
-}
-
-void task2() {
-    #pragma omp parallel num_threads(1)
-    {
-        #pragma omp parallel num_threads(1) if(omp_get_num_threads() > 1)
-        {
-            printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
-        }
-    }
-}
-
 int main() {
-    #pragma omp parallel sections num_threads(8)
+    #pragma omp sections
     {
         #pragma omp section
         {
-            task1();
+            #pragma omp parallel num_threads(3)
+            {
+                #pragma omp if(omp_get_num_threads() > 1)
+                {
+                    if (omp_get_num_threads() > 1) {
+                        printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+                    };
+                }
+            }
         }
 
         #pragma omp section
         {
-            task2();
+            #pragma omp parallel num_threads(1)
+            {
+                #pragma omp if(omp_get_num_threads() > 1)
+                {
+                    if (omp_get_num_threads() > 1) {
+                        printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
+                    };
+                }
+            }
         }
     }
     return 0;
